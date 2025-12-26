bd997188334371aaeb5292c1defb7cc0bc012c45
ahmed <ahmedhosni803@gmail.com>
2025-12-26 18:37:05 +0200

    change color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -6,5 +6,6 @@ class AppColors {
   AppColors._();
   static const Color primary = Color(0xffE2BE7F);
   static const Color white = Colors.white;
-  static const Color dark = Colors.black;
+
+  static const Color dark = Colors.red;
 }
